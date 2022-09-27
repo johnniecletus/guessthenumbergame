d72b9f27d4754487ed23fb008e24f1feefae21a0
John Cletus <71078838+johnniecletus@users.noreply.github.com>
2022-09-27 20:47:04 +0100

    new

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 let hiddenNumber = document.querySelector(".number").textContent = 10;
 const again = document.querySelector(".again").textContent;
-let guess = document.querySelector('.guess').value = 10;
+let guess = document.querySelector('.guess').value = 16;
 let message = document.querySelector(".message").textContent;
 let score = document.querySelector('.score');
 let highScore = document.querySelector('.highScore');
@@ -11,6 +11,6 @@ let highScore = document.querySelector('.highScore');
 let check = document.querySelector(".check").addEventListener('click',function(){
     if(guess === hiddenNumber){
         message = 'correct guess';
-        
+
     }
 });
