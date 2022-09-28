206fc3e1c558f8f6954ce3457b48ddfcf140496a
John Cletus <71078838+johnniecletus@users.noreply.github.com>
2022-09-28 02:27:08 +0100

    new

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-// 'use strict';
+'use strict';
+let score  = 10;
 const again = document.querySelector(".again").textContent;
 const hiddenNumber = Math.trunc(Math.random()*10)+1;
-document.querySelector(".number").textContent = hiddenNumber;
-var score = document.querySelector('.score');
 var highScore = document.querySelector('.highScore');
 
 
 
-const check = document.querySelector(".check").addEventListener('click', function () {    
-var guess = Number(document.querySelector('.guess').value);
-var message = document.querySelector('.message').textContent;
-    if(!guess){
-        message = 'no guess done!!!';}
-    // }else if(guess === hiddenNumber){
-    //     message = 'correct guess!!!';
-    // }else{
 
-    // }
-});
+document.querySelector(".check").addEventListener('click', function () {    
+const guess = Number(document.querySelector('.guess').value);
+let message = document.querySelector('.message');
+let scoreUpdate = document.querySelector('.score');
+const body = document.querySelector('body');
+let numsize = document.querySelector('.number');
+
+if(!guess){
+    message.textContent = '😶no guess done!';
+    }else if(guess === hiddenNumber){
+    message.textContent = '✅ correct guess!';
+        body.classList.add('body');
+        numsize.classList.add('numbers');
+        document.querySelector(".number").textContent = hiddenNumber;
+    }else if(guess<hiddenNumber){
+        if (score>1){
+            message.textContent = '📉 Too low!!!';
+            score--;
+            scoreUpdate.textContent = score;
+        }else{
+            message.textContent = '😈 You lost the game'; 
+            scoreUpdate.textContent = 0;
+        }
+    }else if(guess>hiddenNumber){
+        if (score>1){
+            message.textContent = '📈 Too high!!!';
+            score--;
+            scoreUpdate.textContent = score;
+        }else{
+            message.textContent = '😈 You lost the game'; 
+            scoreUpdate.textContent = 0;
+        }
+        }else{
+
+        }
+    }
+);
